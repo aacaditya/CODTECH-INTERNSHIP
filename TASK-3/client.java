02113f3ef2df2c0c8846b8d4a080b0ada5cea170
aacaditya <adityachaudhari9102004@gmail.com>
2026-01-14 13:13:14 +0530

    Update client.java

--- TASK-3/client.java
+++ TASK-3/client.java
@@ -1,1 +1,35 @@
+import java.io.*;
+import java.net.*;
 
+public class Client {
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("localhost", 5000)) {
+
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            // Thread to read messages from server
+            new Thread(() -> {
+                String serverMessage;
+                try {
+                    while ((serverMessage = in.readLine()) != null) {
+                        System.out.println(serverMessage);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            // Main thread to send messages to server
+            String message;
+            while ((message = consoleReader.readLine()) != null) {
+                out.println(message);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
