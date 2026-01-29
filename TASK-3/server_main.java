9967544d4a8250fb2311378dbbb4312cca2b2ed0
aacaditya <adityachaudhari9102004@gmail.com>
2026-01-29 14:38:44 +0530

    Update server_main.java

--- TASK-3/server_main.java
+++ TASK-3/server_main.java
@@ -1,1 +1,14 @@
+package serverr;
+
+/**
+ *
+ * @author Aditya
+ */
+public class ServerMain {
+    public static void main(String[] args) {
+      Server s=  new Server();
+      s.waitingForClient();
+      s.setIOStream();
+    }
+}
 
